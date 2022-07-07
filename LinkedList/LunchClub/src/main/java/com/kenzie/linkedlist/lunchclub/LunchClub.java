c0bbb36dca0e6195c951dbf268ea0a2437143cbc
NicholeD <YOUREMAIL@YOUREMAIL.com>
2022-07-06 21:39:38 -0600

    Implemented LunchClub::addMemberToLine:addGroupToLine:ServeMember:getMembersInLine. Tests pass.

--- LinkedList/LunchClub/src/main/java/com/kenzie/linkedlist/lunchclub/LunchClub.java
+++ LinkedList/LunchClub/src/main/java/com/kenzie/linkedlist/lunchclub/LunchClub.java
@@ -17,21 +17,21 @@ public class LunchClub {
 
     // -- Simulates someone entering the back of the line for lunch.
     public void addMemberToLine(String member){
-        // TODO: add Member to membersInLine on the next line
-
+        membersInLine.add(member);
     }
 
     //-- Simulates a group of people entering and adding to the back of the line.
     public void addGroupToLine(List<String> group) {
-        // TODO: add a group of Members to membersInLine on the next line
+        for (String member : group) {
+            membersInLine.add(member);
+        }
     }
 
     //-- Removes a person from the front of the lunch line to serve them food.
     // do not attempt to remove a person if membersInLine is empty
     public String serveMember(){
         try{
-            // TODO: remove First Member from membersInLine to replace the next line of code
-            return "";
+            return membersInLine.remove();
         } catch (NoSuchElementException e) {
             System.out.println(e.getMessage());
             return "";
@@ -40,7 +40,6 @@ public class LunchClub {
 
     // --Returns the List of members representing the current state of the lunch line.
     public LinkedList<String> getMembersInLine() {
-        // TODO: return membersInLine;
-        return new LinkedList<>();
+        return membersInLine;
     }
 }
